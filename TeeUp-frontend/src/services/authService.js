d18d8d5f0b891c8d2b6b3494b5eab9be7f2e9e6e
zoon <kevinjo11@naver.com>
2025-07-06 12:04:31 +0900

    팔로우 및 search

--- TeeUp-frontend/src/services/authService.js
+++ TeeUp-frontend/src/services/authService.js
@@ -3,10 +3,10 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { API_BASE_URL } from '@env'; // 스프링 서버의 URL을 적어주세요.
 
 //로그인 요청
-export const login = async (userid, password) => {
+export const login = async (loginId, password) => {
   try {
     const response = await axios.post(`${API_BASE_URL}/api/auth/login`, {
-      userid,
+      loginId,
       password,
     });
     console.log('로그인 요청 성공');
@@ -14,6 +14,7 @@ export const login = async (userid, password) => {
 
     try {
       await AsyncStorage.setItem('userToken', token);
+      await AsyncStorage.setItem('loginId', user.loginId);
       console.log('토큰 저장 성공:', token);
     } catch (e) {
       console.error('❌ 토큰 저장 실패:', e);
@@ -27,13 +28,13 @@ export const login = async (userid, password) => {
 };
 
 // 회원가입 요청
-export const signup = async ({ userid, password, nickname }) => {
+export const signup = async ({ loginId, password, nickname }) => {
   console.log('회원가입 요청 시도');
   console.log('회원가입 BASE_URL:', API_BASE_URL);
 
   try {
     const response = await axios.post(`${API_BASE_URL}/api/auth/signup`, {
-      userid,
+      loginId,
       password,
     });
     console.log('회원가입 요청 성공');
